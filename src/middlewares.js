c1f94f9dbef88b95ea4aee47300a5d0f134a8560
Laisses <laisse@gmail.com>
2022-12-19 12:49:46 -0300

    src/middlewares.js: add schema validation function

--- src/middlewares.js
+++ src/middlewares.js
@@ -11,3 +11,18 @@ export const asyncError = handlerFn = async (req, res, next) => {
         });
     }
 };
+
+export const validate = schema => (req, res, next) => {
+    const payload = req.body;
+    const {error} = validator(schema, payload);
+
+    if (error) {
+        const errors = error.details.map(detail => detail.message);
+        return res.status(422).send({
+            message: "Unprocessable Entity",
+            errors,
+        });
+    }
+
+    next();
+}
