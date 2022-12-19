c48a7d76c3f52710f246757e0fe4527e81b23b11
Laisses <laisse@gmail.com>
2022-12-19 13:14:31 -0300

    src/routes.js: create POST signUp route

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,9 @@
-export const routes = () => {
+import * as h from "./handlers.js";
+import * as m from "./middlewares.js";
+import * as s from "./schemas.js";
 
+
+export const routes = app => {
+
+    app.post("/signup", m.validate(s.user), m.validateEmail, m.asyncError(h.addNewUser));
 };
