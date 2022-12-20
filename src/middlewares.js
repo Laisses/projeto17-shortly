3c539f856d709c52f7e494611d3bf6649d96003c
Laisses <laisse@gmail.com>
2022-12-20 10:13:17 -0300

    src/middlewares.js: remove unused console.log

--- src/middlewares.js
+++ src/middlewares.js
@@ -78,7 +78,6 @@ export const authenticate = async (req, res, next) => {
     }
 
     const session = await connection.query(`SELECT * FROM sessions WHERE token=$1;`, [token]);
-    console.log(session);
     const user = await connection.query(`SELECT * FROM users WHERE id=$1;`, [session.rows[0]?.user_id]);
 
     if (!user.rows[0]) {
